5e31c74b663632da66002e4bc0db5889f6ddce0d
Hans Meine <hans_meine@gmx.net>
2023-05-22 21:02:24 +0200

    start parsing page table

--- src/parser.rs
+++ src/parser.rs
@@ -4,8 +4,9 @@ use nom::{
     bytes::complete::{is_not, tag},
     character::complete as cc,
     combinator::map,
-    multi::many0,
-    sequence::{pair, preceded, terminated},
+    multi::{count, many0},
+    number::complete as nc,
+    sequence::{pair, preceded, terminated, tuple},
     IResult,
 };
 
@@ -13,6 +14,16 @@ pub struct VersionHeader {
     pub major: u16,
     pub minor: u16,
     pub patch: u16,
+}
+
+pub struct PageIdxEntry {
+    pub start_offset: u64,
+    pub end_offset: u64,
+    pub is_compressed: bool,
+    pub checksum: u32, // 24 bit
+    pub flag_byte: u8,
+    pub internal: [u8; 11],
+    pub default_voxel_value: u16,
 }
 
 pub struct MLImage {
@@ -60,6 +71,37 @@ pub fn tag_list(input: &[u8]) -> IResult<&[u8], Vec<(String, String)>> {
     many0(tag_pair)(input)
 }
 
+// see http://mevislabdownloads.mevis.de/docs/current/MeVisLab/Resources/Documentation/Publish/SDK/ToolBoxReference/mlImageFormatIdxTable_8h_source.html
+pub fn parse_page_idx_entry(input: &[u8]) -> IResult<&[u8], PageIdxEntry> {
+    let (input, (start_offset, end_offset, is_compressed)) =
+        tuple((nc::le_u64, nc::le_u64, nc::u8))(input)?;
+    let is_compressed = is_compressed > 0;
+
+    let (input, (checksum_low, checksum_med, checksum_high, flag_byte)) =
+        tuple((nc::u8, nc::u8, nc::u8, nc::u8))(input)?;
+    let checksum =
+        (checksum_low as u32) | (checksum_med as u32) << 8 | (checksum_high as u32) << 16;
+
+    let (input, internal) = count(nc::u8, 11)(input)?;
+    let internal: [u8; 11] = internal.try_into().unwrap();
+
+    // FIXME: depends on voxel type!
+    let (input, default_voxel_value) = nc::le_u16(input)?;
+
+    Ok((
+        input,
+        PageIdxEntry {
+            start_offset,
+            end_offset,
+            is_compressed,
+            checksum,
+            flag_byte,
+            internal,
+            default_voxel_value,
+        },
+    ))
+}
+
 pub fn parse_file(input: &[u8]) -> IResult<&[u8], MLImage> {
     let (tag_list_input, version) = version_header(input)?;
 
@@ -67,6 +109,8 @@ pub fn parse_file(input: &[u8]) -> IResult<&[u8], MLImage> {
 
     let (tag_list_input, rest) = tag_list_input.split_at(tag_list_size);
     let (nothing, tag_list) = tag_list(tag_list_input)?;
+
+    dbg!(&tag_list);
 
     Ok((input, MLImage { version, tag_list }))
 }
@@ -123,7 +167,7 @@ mod tests {
         let asset = include_bytes!("../assets/test_32x32x8.mlimage");
         let result = parse_file(asset);
         assert!(result.is_ok());
-        if let Some((rest, image)) = result.ok() {
+        if let Some((_rest, image)) = result.ok() {
             assert_eq!(image.version.major, 0);
             assert_eq!(image.version.minor, 1);
         }
